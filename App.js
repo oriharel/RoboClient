2a08766856127f5511a1b107ac03afb622c9d65e
Ori Harel <ori.harel@gmail.com>
2019-03-07 22:46:13 +0200

    nice alpha

--- App.js
+++ App.js
@@ -1,11 +1,93 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
 
-export default class App extends React.Component {
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  WebView,
+  Dimensions
+} from "react-native";
+
+const baseUrl = "http://192.168.1.24:5000";
+
+const deviceWidth = Dimensions.get("window").width;
+
+export default class App extends Component {
+  state = {
+    forward: false,
+    backward: false,
+    left: false,
+    right: false
+  };
+  go = direction => {
+    console.log(`Going...`);
+    this.setState({});
+    fetch(`${baseUrl}/${direction}`).then(() => {});
+  };
+
+  stop = () => {
+    console.log(`Stopping...`);
+    fetch(`${baseUrl}/stop`).then(() => {});
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <WebView
+          source={{ uri: "http://192.168.1.24:8082" }}
+          style={{ width: deviceWidth }}
+        />
+        <View style={styles.subContainer}>
+          <View style={styles.row}>
+            <TouchableWithoutFeedback
+              onPressIn={() => {
+                this.go("forward");
+              }}
+              onPressOut={this.stop}
+            >
+              <Text style={styles.button}>Forward</Text>
+            </TouchableWithoutFeedback>
+          </View>
+          <View style={styles.row}>
+            <TouchableWithoutFeedback
+              onPressIn={() => {
+                this.go("left");
+              }}
+              onPressOut={this.stop}
+            >
+              <Text style={styles.button}>Left</Text>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={this.stop}>
+              <Text style={styles.button}>Stop</Text>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback
+              onPressIn={() => {
+                this.go("right");
+              }}
+              onPressOut={this.stop}
+            >
+              <Text style={styles.button}>Right</Text>
+            </TouchableWithoutFeedback>
+          </View>
+          <View style={styles.row}>
+            <TouchableWithoutFeedback
+              onPressIn={() => {
+                this.go("backward");
+              }}
+              onPressOut={this.stop}
+            >
+              <Text style={styles.button}>Backward</Text>
+            </TouchableWithoutFeedback>
+          </View>
+        </View>
       </View>
     );
   }
@@ -14,8 +96,29 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000"
   },
+  subContainer: {
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  subContainerPressed: {
+    backgroundColor: "#000"
+  },
+  row: {
+    flexDirection: "row"
+  },
+  button: {
+    width: 100,
+    height: 100,
+    backgroundColor: "grey",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    textAlignVertical: "center",
+    margin: 15
+  }
 });
